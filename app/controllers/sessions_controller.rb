78dd87ae1cfa6f4cb4189845e8c57c0139862b5b
jrajan20 <jai_20j@hotmail.com>
2018-11-12 00:47:36 +0000

    order details added, views for order show complete with bootstrap help from Adi Bakija

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-  	@user = User.find_by_email(params[:email])
-  	if @user && @user.authenticate(params[:password])
-  		session[:user_id] = @user.id
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
   		redirect_to '/', notice: "Logged in!"
   	else  		
   		redirect_to "/login"
